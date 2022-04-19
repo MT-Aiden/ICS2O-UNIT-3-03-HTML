7ea73d12137535fcc5d589cf0f7fc0e15ff9983d
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-04-19 02:16:07 +0000

    javascript

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function calculate() {
 
   // process
 
-  const volume = Math.PI * 3 / 4 * Math.pow(radius, 3)
+  const volume = (4/3)* Math.PI * Math.pow(radius, 3);
 
   // output
   document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm³'
